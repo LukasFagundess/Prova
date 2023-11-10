5822780406134701adb05ab90619f3ccf7a98ab4
LukasFagundess <134522312+LukasFagundess@users.noreply.github.com>
2023-11-10 12:36:23 -0300

    .

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function mudarTexto() {
     document.getElementById("demo").innerHTML = "Texto alterado!";
 }
 
-<style>
-body{
- background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1Kz1ppTGueudzjM_LMhd7dvCkoXMaz5PG3igQHcBsqhmru2L8vbsRGKQ4YS9_oOLBpr8&=);
-}     
-</style>
+     <style>
+      body{
+      background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1Kz1ppTGueudzjM_LMhd7dvCkoXMaz5PG3igQHcBsqhmru2L8vbsRGKQ4YS9_oOLBpr8&=);
+         }         
+      </style>
