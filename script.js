2b253947e257c82ff63c7de7eb90a4b89c6e6b0b
LukasFagundess <134522312+LukasFagundess@users.noreply.github.com>
2023-11-10 12:33:16 -0300

    .,

--- script.js
+++ script.js
@@ -2,6 +2,3 @@ function mudarTexto() {
     document.getElementById("demo").innerHTML = "Texto alterado!";
 }
 
-body{
-    background-image: url(https://www.tibiabr.com/wp-content/uploads/2018/04/TrollFace.jpg);
-   }    
